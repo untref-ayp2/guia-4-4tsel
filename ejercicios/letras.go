b0a00006915aea80bbb2f76bdac2063580e4cbd1
4tsel <axelbnovo@gmail.com>
2023-04-20 08:39:35 -0300

    Cambios grales

--- ejercicios/letras.go
+++ ejercicios/letras.go
@@ -1,9 +1,24 @@
 package ejercicios
 
 import (
+	"github.com/agrison/go-commons-lang/stringUtils"
 	"guia4/set"
 )
 
 func Letras(s string) *set.Set[string] {
-	panic("Not implemented")
+
+	result := set.NewSet[string]()
+
+	for _, v := range s {
+
+		letra := string(v)
+
+		if !stringUtils.IsBlank(letra) {
+
+			result.Add(letra)
+		}
+
+	}
+
+	return result
 }
--- main.go
+++ main.go
@@ -3,16 +3,18 @@ package main
 import (
 	"fmt"
 	"guia4/ejercicios"
-	"guia4/set"
+	// "guia4/set"
 )
 
 func main() {
 	// Creo un conjunto de letras
 	letras := ejercicios.Letras("Hola Mundo")
 	fmt.Println(letras)
+	arreglo := []int{1, 4, 5, 2, 1, 4, 5, 6}
+	fmt.Println(ejercicios.EliminarRepetidos(arreglo))
 	// Creo dos conjuntos de números
-	n1 := set.NewSet(1, 10, 5)
+	/* n1 := set.NewSet(1, 10, 5)
 	n2 := set.NewSet(5, 15, 1)
 	fmt.Println("Diferencia Simetrica: ", ejercicios.DiferenciaSimetrica(n1, n2))
-	fmt.Println("Diferencia: ", set.Difference(n1, n2))
+	fmt.Println("Diferencia: ", set.Difference(n1, n2)) */
 }

ed6c28725827273447ab36d5901827fa1550d90c
Axel N <axelbnovo@gmail.com>
2023-05-03 22:43:30 -0300

    Modificado main.go

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func main() {
 	n1 := set.NewSet(1, 10, 5)
 	n2 := set.NewSet(5, 15, 1)
 	n3 := set.NewSet(10, 0, 2)
+	fmt.Println(n1)
 	fmt.Println("Diferencia Simetrica: ", ejercicios.DiferenciaSimetrica(n1, n2))
 	fmt.Println("Diferencia: ", set.Difference(n1, n2))
 
